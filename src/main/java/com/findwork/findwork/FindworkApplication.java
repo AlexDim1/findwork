c0922aa513aaf6f392f623ae518d7dd058e4f083
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-10-20 18:54:09 +0300

    Initial commit

---
+++ src/main/java/com/findwork/findwork/FindworkApplication.java
@@ -0,0 +1,13 @@
+package com.findwork.findwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findwork/findwork/FindworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findwork.findwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
