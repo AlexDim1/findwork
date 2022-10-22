df22f193be0c395262576dff08a345a4bb650b86
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-10-22 15:28:43 +0300

    Created JobOffer Entity

---
+++ src/main/java/com/findwork/findwork/Entities/JobOffer.java
@@ -0,0 +1,82 @@
+package com.findwork.findwork.Entities;
+
+import com.findwork.findwork.Enums.JobLevel;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+public class JobOffer {
+    @Id
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT", length = 2000)
+    private String description;
+
+    private String location;
+
+    private String salary;
+
+    @Enumerated
+    private JobLevel jobLevel;
+
+    public JobOffer() {
+    }
+
+    public JobOffer(String title, String description, String location, String salary, JobLevel jobLevel) {
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.salary = salary;
+        this.jobLevel = jobLevel;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public JobLevel getLevel() {
+        return jobLevel;
+    }
+
+    public void setLevel(JobLevel jobLevel) {
+        this.jobLevel = jobLevel;
+    }
+}
---
+++ src/main/java/com/findwork/findwork/Enums/JobLevel.java
@@ -0,0 +1,8 @@
+package com.findwork.findwork.Enums;
+
+public enum JobLevel {
+    INTERN,
+    JUNIOR,
+    MID,
+    SENIOR
+}
