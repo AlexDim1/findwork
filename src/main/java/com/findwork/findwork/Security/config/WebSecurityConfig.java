b4bb1247d82e42f7c38d27b66019d738cdf29a0c
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-11-04 17:19:26 +0200

    More security setup

--- src/main/java/com/findwork/findwork/Security/config/WebSecurityConfig.java
+++ src/main/java/com/findwork/findwork/Security/config/WebSecurityConfig.java
@@ -1,16 +1,46 @@
 package com.findwork.findwork.Security.config;
 
+import com.findwork.findwork.Security.PasswordEncoder;
+import com.findwork.findwork.Services.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    private final UserService userService;
+    private final BCryptPasswordEncoder encoder;
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().permitAll();
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encoder);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
+
+    public WebSecurityConfig(UserService userService, BCryptPasswordEncoder encoder) {
+        this.userService = userService;
+        this.encoder = encoder;
     }
 }
