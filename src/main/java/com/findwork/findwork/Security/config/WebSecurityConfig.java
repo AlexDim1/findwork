e7bf3f283892701572ce79338b60dcac5fddfb40
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-11-04 16:43:18 +0200

    Created security config

---
+++ src/main/java/com/findwork/findwork/Security/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.findwork.findwork.Security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+
+        return http.build();
+    }
+}
