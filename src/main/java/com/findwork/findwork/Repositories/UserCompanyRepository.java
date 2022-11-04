9796fbcf7ad922c0ead04dd310d0958973f8cb9d
Alexander Dimitrov <72303198+AlexDim1@users.noreply.github.com>
2022-11-04 16:07:22 +0200

    Changed method signature

--- src/main/java/com/findwork/findwork/Repositories/UserCompanyRepository.java
+++ src/main/java/com/findwork/findwork/Repositories/UserCompanyRepository.java
@@ -3,9 +3,8 @@ package com.findwork.findwork.Repositories;
 import com.findwork.findwork.Entities.Users.UserCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserCompanyRepository extends JpaRepository<UserCompany, UUID> {
-    Optional<UserCompany> findUserCompanyByUsername(String username);
+    UserCompany findUserCompanyByUsername(String username);
 }
