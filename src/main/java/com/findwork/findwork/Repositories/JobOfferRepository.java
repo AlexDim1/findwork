c691b07bcec52f1841c4c0508777b44a9378636a
De221 <denits221@abv.bg>
2022-12-01 00:50:41 +0200

    Some filters

--- src/main/java/com/findwork/findwork/Repositories/JobOfferRepository.java
+++ src/main/java/com/findwork/findwork/Repositories/JobOfferRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 public interface JobOfferRepository extends JpaRepository<JobOffer, UUID> {
@@ -14,4 +13,38 @@ public interface JobOfferRepository extends JpaRepository<JobOffer, UUID> {
     JobOffer findJobOfferById(UUID id);
 
     List<JobOffer> findJobOfferByCompany(UserCompany company);
+
+    /*@Query("SELECT o " +
+            "FROM JobOffer o " +
+            "WHERE o.requirements = :taskId")
+    List<JobOffer> filterOffersByRequirements(String);*/
+
+    @Query("select j from JobOffer j where lower(j.benefits) like lower(concat('%', ?1, '%'))")
+    List<JobOffer> findJobOffersByBenefitsContaining(String benefits);
+
+    @Query("select j from JobOffer j where lower(j.description) like lower(concat('%', ?1, '%'))")
+    List<JobOffer> findJobOffersByDescriptionContaining(String description);
+
+    @Query("select j from JobOffer j where concat(j.jobCategory,'')  like %?1%")
+    List<JobOffer> findJobOffersByJobCategoryContaining(String jobCategory); // jobCategory се въвежда като число, т.е: 1 --> Commerce
+
+    @Query("select j from JobOffer j where concat(j.jobLevel,'')  like %?1%")
+    List<JobOffer> findJobOffersByJobLevelContaining(String jobLevel); // jobLevel се въвежда като число, т.е: 1 --> Junior
+
+    @Query("select j from JobOffer j where lower(j.location) like lower(concat('%', ?1, '%'))")
+    List<JobOffer> findJobOffersByLocationContaining(String location);
+
+    @Query("select j from JobOffer j where lower(j.niceToHave) like lower(concat('%', ?1, '%'))")
+    List<JobOffer> findJobOffersByNiceToHaveContaining(String niceToHave);
+
+    @Query("select j from JobOffer j where lower(j.requirements) like lower(concat('%', ?1, '%'))")
+    List<JobOffer> findJobOffersByRequirementsContaining(String requirements);
+
+    @Query("select j from JobOffer j where CAST(j.salary AS int) >= CAST(?1 AS int) AND CAST(j.salary AS int) <= CAST(?2 AS int)")
+    List<JobOffer> findJobOffersBySalary(String lowEnd, String highEnd);
+
+    //@Query("select j from JobOffer j where lower(j.title) in (Select column_value FROM table(sys.odcivarchar2list(?1)))")
+    //List<JobOffer> findJobOffersByKeyWords(String[] keyWords);
+
+
 }
